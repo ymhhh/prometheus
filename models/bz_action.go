bd2e9ce1e19cf5862af79b5ffb7e66707f0332b3
huanghonghu <huanghonghu@jd.com>
2020-03-25 17:34:45 +0800

    增加对groupby, repeat_interval 数据库的配置

--- models/bz_action.go
+++ models/bz_action.go
@@ -1,7 +1,7 @@
 package models
 
 type BzAction struct {
-	Id         int    `xorm:"not null pk autoincr INT(11)"`
-	ActionType int    `xorm:"not null default 0 index INT(11)"`
+	Id         int32  `xorm:"not null pk autoincr INT(11)"`
+	ActionType int32  `xorm:"not null default 0 index INT(11)"`
 	Name       string `xorm:"not null VARCHAR(50)"`
 }
--- models/bz_monitor_rel.go
+++ models/bz_monitor_rel.go
@@ -5,14 +5,14 @@ import (
 )
 
 type BzMonitorRel struct {
-	Id        int       `xorm:"not null pk INT(11)"`
-	Type      int       `xorm:"INT(11)"`
+	Id        int32     `xorm:"not null pk INT(11)"`
+	Type      int32     `xorm:"INT(11)"`
 	RefId     string    `xorm:"VARCHAR(50)"`
 	MonitorId string    `xorm:"not null default '' index VARCHAR(50)"`
 	Name      string    `xorm:"not null default '' VARCHAR(50)"`
 	CreatedAt time.Time `xorm:"DATETIME"`
 	UpdatedAt time.Time `xorm:"DATETIME"`
-	Deleted   int       `xorm:"INT(11)"`
-	SystemId  int       `xorm:"INT(11)"`
-	ProcessId int       `xorm:"INT(11)"`
+	Deleted   int32     `xorm:"INT(11)"`
+	SystemId  int32     `xorm:"INT(11)"`
+	ProcessId int32     `xorm:"INT(11)"`
 }
